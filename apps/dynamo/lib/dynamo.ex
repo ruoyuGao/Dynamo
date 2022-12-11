aa4e05ab7f51c017deac3d0c0ce33d31a6ca54f0
ruoyuGao <ruoyu.gao@nyu.edu>
2022-12-10 19:37:19 -0500

    upload dynamo struct without physical node

--- apps/dynamo/lib/dynamo.ex
+++ apps/dynamo/lib/dynamo.ex
@@ -16,44 +16,107 @@ defmodule Dynamo do
   # if you are interested in this. Note we currently purge all logs
   # below Info
   require Logger
-  @spec hash_function(String.t()) :: String.t()
-  def hash_function(meta_data) do
-    result = MerkleTree.Crypto.hash(meta_data,:md5)
-    result
-  end
-
   defstruct(
-    hash_table: nil,
-    hash_tree: nil,
+    hash_table: nil, #store <key,<value,value_vector_clock,is_replica,hash_code>>
+    hash_trees_map: nil, #use to store several hash tree for several key range
+    key_range_map: nil, # store <ab: [1,3]>
+    message_table: nil,#same as message_list, used for gossip
     prefer_list: nil,
-    vector_clock: nil, #same as message_list
+    check_alive_timer: nil, # is much longer than heartbeat timer, in order to check liveness of prefer list
+    response_list: nil, #store which node(in prefer_list)has send heartbeat response
     heartbeat_timer: nil,
     heartbeat_timeout: nil
   )
   @spec new_configuration(
     [atom()],
+    %{},
     non_neg_integer()
   ):: %Dynamo{}
   def new_configuration(
     prefer_list,
+    key_range_map,
     heartbeat_timeout
   ) do
     %Dynamo{
       hash_table: Map.new(),
-      hash_tree: MerkleTree.new(["placeholder"],&hash_function/1),
-      prefer_list: [],
-      vector_clock: [],
-      heartbeat_timeout: heartbeat_timeout
-    }
+      hash_trees_map: %{},#{a: 1, b: 1, c:1}
+      message_table: %{},#{a:1}
+      prefer_list: prefer_list, #{:a,:b,:c}
+      response_list: [], #{:a,:b,:c}
+      key_range_map: key_range_map, #{ab:[1,3], bc:[4,6]}
+      heartbeat_timeout: heartbeat_timeout}
   end
-  @spec physical_node(%Dynamo{},any)::no_return()
-  def physical_node(state,extra_state) do
-    IO.puts("waiting for write")
-  end
-
   @spec virtual_node(%Dynamo{},any)::no_return()
   def virtual_node(state,extra_state) do
-    IO.puts("waiting for write")
+    receive do
+      {sender, %Dynamo.PutEntryRequest{
+        key: key,
+        value: value,
+        hash_code: hash_code,
+        value_vector_clock: value_vector_clock
+      }} ->
+        # if vector is earlier than current object vector_clock, do not update
+        # else update the object in hash_table
+
+        #use hash_code to search which key range it belongs to, reconstruct the hash_tree
+
+        #send PutEntryResponse to coordinate node
+        "wait to write"
+      {sender, %Dynamo.PutEntryResponse{
+        hash_code: hash_code,
+        success: success
+      }} ->
+        #use hash_code to identify the key_value pair
+        # use map in extra state to make sure if get more than W response, return message to client
+        raise "wait to write"
+      {sender, %Dynamo.GetEntryRequest{
+        key: key,
+        hash_tree: hash_tree
+      }} ->
+        #send GetEntryResponse to sender(2cond: tree is same or not)
+        raise "wait to write"
+      {sender,%Dynamo.GetEntryResponse{
+        key: key,
+        value: value,
+        is_same: is_same,
+        value_vector_clock: value_vector_clock
+      }} ->
+        #if receive more than R response, send result to client
+        raise "wait to write"
+      {sender, {:get, key}} ->
+        #receive get request from client
+        #if the node store this key and it is not replica, broadcast GetEntryRequest
+        #else transfer this message to the next node in prefer list
+        raise "wait to write"
+      {sender, {:put, key, value, hash_code}} ->
+        #coordinate node receive put request from client, broadcast PutEntryRequest to prefer list
+        raise "wait to write"
+      :heartbeat_timer ->
+        #When heartbeat is timeout, reset timer and broadcast to prefer list
+        raise "wait to write"
+      {sender, :heartbeat} ->
+        # receiver heartbeat from other nodes, send ok to that node
+        send(sender, :alive)
+      {sender, :alive} ->
+        #receive response from alive nodes in prefer list
+        #add the sender in prefer list
+        raise "wait to write"
+      :check_alive_timer ->
+        # check if we have received every response from prefer list
+        # if response_list == prefer_list, reset response_list
+        # else start gossip
+        #send {:gossip, dead_node} to other nodes(except the next node of dead_node)
+        #send {:gossip_reconfig, dead_node} to the next node for dead_node
+        raise "wait to write"
+      {sender,{:gossip_reconfig, dead_node}} ->
+        #change all the data in the coordinate key range for dead_node to this node(the previous range of current node coordinate range)
+        #notice physical node that coordinate range of dead_node is replaced by current node
+        raise "wait to write"
+      {sender, {:gossip, dead_node}} ->
+        # if dead_node in the message list and not in prefer list, ignore it
+        # else delete this node in prefer_list, and add its in message_list
+        raise "wait to write"
+    end
   end
 end
 
@@ -64,10 +127,32 @@ defmodule Dynamo.Client do
     except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
 
   alias __MODULE__
-  defstruct(node_list: nil)
-
-  @spec new_client(%{}) :: %Client{node_list: %{}}
+  defstruct(
+    node_list: nil,
+    )
+  #node_list store node name and its position on the ring
+  @spec new_client(list(any())) :: %Client{node_list: list(any())}
   def new_client(node_list) do
     %Client{node_list: node_list}
+  end
+
+  @spec put(%Client{},non_neg_integer(),non_neg_integer())::{:ok, %Client{}}
+  def put(client, key, value) do
+    hash_code = key + value # use hash function later
+    #check node_map to find coordinate node
+    #send put to coordinate node
+    receive do
+      {_, :ok} ->
+        {:ok, client}
+    end
+  end
+
+  @spec get(%Client{},non_neg_integer())::{:ok,{non_neg_integer(), %Client{}}}
+  def get(client, key) do
+    #send get request to physical node
+    receive do
+      {_, value} ->
+        {value,client}
+      end
   end
 end
---
+++ apps/dynamo/lib/dynamo_message.ex
@@ -0,0 +1,159 @@
+defmodule Dynamo.ObjectEntry do
+  @moduledoc """
+  Object entry for Dyname, contain object_value, object_version(vector_clock), is_replica, hash_code
+  """
+  alias Dynamo.ObjectEntry
+  alias __MODULE__
+  @enforce_keys [:value, :value_vector_clock, :is_replica, :hash_code]
+  defstruct(
+    value: nil,
+    value_vector_clock: nil,
+    is_replica: nil,
+    hash_code: nil,
+  )
+  @doc """
+  Return a ObjecyEntry for putting in to the hash_table
+  """
+  @spec putObject(non_neg_integer(), %{}, non_neg_integer(), non_neg_integer()):: %ObjectEntry{
+    value: non_neg_integer(),
+    value_vector_clock: %{},
+    is_replica: non_neg_integer(),
+    hash_code: non_neg_integer(),
+  }
+  def putObject(value, value_vector_clock, is_replica, hash_code) do
+    %ObjectEntry{
+      value: value,
+      value_vector_clock: value_vector_clock,
+      is_replica: is_replica,
+      hash_code: hash_code
+    }
+  end
+end
+
+
+defmodule Dynamo.PutEntryRequest do
+  @moduledoc """
+  Put(key,context,object) RPC request
+  coordinate node send this RPC to replica node
+  """
+  alias __MODULE__
+  @enforce_keys [:key,
+    :value,
+    :hash_code,
+    :value_vector_clock
+  ]
+
+  defstruct(
+    key: nil,
+    value: nil,
+    hash_code: nil,
+    value_vector_clock: nil
+  )
+  @doc """
+  Create a new PutEntryRequest
+  """
+  @spec new(
+    non_neg_integer(),
+    non_neg_integer(),
+    non_neg_integer(),
+    map()
+  ):: %PutEntryRequest{
+    key: non_neg_integer(),
+    value: non_neg_integer(),
+    hash_code: non_neg_integer(),
+    value_vector_clock: map()
+  }
+  def new(key, value, hash_code, value_vector_clock) do
+    %PutEntryRequest{
+      key: key,
+      value: value,
+      hash_code: hash_code,
+      value_vector_clock: value_vector_clock
+    }
+  end
+end
+
+defmodule Dynamo.PutEntryResponse do
+  @moduledoc """
+  Response for the PutEntryRequest
+  """
+  alias __MODULE__
+  @enforce_keys [:hash_code, :success]
+  defstruct(
+    hash_code: nil,
+    success: nil
+  )
+
+  @doc """
+  Create a new PutEntryResponse
+  """
+alias Dynamo.PutEntryResponse
+  @spec new(non_neg_integer(),boolean()):: %PutEntryResponse{
+    hash_code: non_neg_integer(),
+    success: boolean()
+  }
+  def new(hash_code, success) do
+    %PutEntryResponse{
+      hash_code: hash_code,
+      success: success
+    }
+  end
+end
+
+defmodule Dynamo.GetEntryRequest do
+  @moduledoc """
+  GetEntry RPC request, coordinate node use this RPC to call replica node
+  """
+  alias __MODULE__
+  @enforce_keys [:key, :hash_tree]
+  defstruct(
+    key: nil,
+    hash_tree: nil
+  )
+
+alias Dynamo.GetEntryRequest
+  @spec new(non_neg_integer(), any()) :: %GetEntryRequest{
+    key: non_neg_integer(),
+    hash_tree: any()
+  }
+  def new(key, hash_tree) do
+    %GetEntryRequest{
+      key: key,
+      hash_tree: hash_tree
+    }
+  end
+end
+
+defmodule Dynamo.GetEntryResponse do
+  @moduledoc """
+  Response for GetEntryRequest
+  """
+  alias Dynamo.GetEntryResponse
+  alias __MODULE__
+  @enforce_keys [:key, :value, :is_same, :value_vector_clock]
+  defstruct(
+    key: nil,
+    value: nil,
+    is_same: nil,
+    value_vector_clock: nil
+  )
+
+  @doc """
+  Create a new GetEntryResponse
+  """
+  @spec new(non_neg_integer(), non_neg_integer(), boolean(), map())::
+  %GetEntryResponse{
+    key: non_neg_integer(),
+    value: non_neg_integer(),
+    is_same: boolean(),
+    value_vector_clock: map()
+  }
+  def new(key, value, is_same, value_vector_clock) do
+    %GetEntryResponse{
+      key: key,
+      value: value,
+      is_same: is_same,
+      value_vector_clock: value_vector_clock
+    }
+  end
+end
