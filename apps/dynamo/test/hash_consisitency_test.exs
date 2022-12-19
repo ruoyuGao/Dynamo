b8f9689009c7492079860489d79a15a8e4752505
juexZZ <954742885@qq.com>
2022-12-19 03:45:03 -0500

    bug fixes to pass the test.

--- apps/dynamo/test/hash_consisitency_test.exs
+++ apps/dynamo/test/hash_consisitency_test.exs
@@ -10,20 +10,25 @@ defmodule HashTest do
     Emulation.init()
     Emulation.append_fuzzers([Fuzzers.delay(2)])
     w = r = n = 1
-    base_config_a = Dynamo.new_configuration([:b, :c], %{a: [240,360], b: [0,120], c: [120, 240]}, 10, :client, w, r, n)
-    base_config_b = Dynamo.new_configuration([:c, :a], %{a: [240,360], b: [0,120], c: [120, 240]}, 10, :client, w, r, n)
-    base_config_c = Dynamo.new_configuration([:a, :b], %{a: [240,360], b: [0,120], c: [120, 240]}, 10, :client, w, r, n)
+    # base_config_a = Dynamo.new_configuration([:b, :c], %{a: [240,360], b: [0,120], c: [120, 240]}, 10, :client, w, r, n)
+    base_config_a = Dynamo.new_configuration([:b, :c], %{a: [0,120], b: [120,240], c: [240, 360]}, 10, :client, w, r, n)
+    base_config_b = Dynamo.new_configuration([:c, :a], %{a: [0,120], b: [120,240], c: [240, 360]}, 10, :client, w, r, n)
+    base_config_c = Dynamo.new_configuration([:a, :b], %{a: [0,120], b: [120,240], c: [240, 360]}, 10, :client, w, r, n)
 
-    spawn(:b, fn -> Dynamo.become_virtual_node(base_config_a) end)
-    spawn(:c, fn -> Dynamo.become_virtual_node(base_config_b) end)
-    spawn(:a, fn -> Dynamo.become_virtual_node(base_config_c) end)
+    base_config_a = Dynamo.new_configuration([:b, :c], %{a: [0,120], b: [120,240], c: [240, 360]}, 10, :client, w, r, n)
+
+
+    spawn(:a, fn -> Dynamo.become_virtual_node(base_config_a) end)
+    spawn(:b, fn -> Dynamo.become_virtual_node(base_config_b) end)
+    spawn(:c, fn -> Dynamo.become_virtual_node(base_config_c) end)
 
     client =
       spawn(:client, fn ->
         client = Dynamo.Client.new_client([:a, :b, :c])
         {:ok, client} = Dynamo.Client.put(client, 5, "a")
-        {{:value, v}, client} = Raft.Client.get(client, 5)
-        assert v == ["a", %{b: 1}]
+        IO.puts("successfully put.")
+        {{:value, v}, client} = Dynamo.Client.get(client, 5)
+        assert v == ["a", %{a: 1}]
       end)
 
     handle = Process.monitor(client)
